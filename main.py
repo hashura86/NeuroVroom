d1ad4235f54be8db3e721f51bdcc7b37d53174c5
João Ribeiro <joaoribeiro8699@gmail.com>
2023-09-12 20:11:20 -0300

    learning about game state + trying to resolve car render bug

--- main.py
+++ main.py
@@ -14,17 +14,13 @@ def check_sin_move(movement):
         y_offset = amplitude * math.sin(frequency * elapsed_time)
         car.rect.y += y_offset 
 
-
 # function to check if generated car is green
 def check_green(color):
     global random_car_color
     while color in random_car_color:
         random_car_color = random.choice(car_colors)
 
-# def draw_scenario(surface, image_path, x, y):
-#     surface_target = pygame.image.load(image_path).convert()
-#     surface.blit(surface_target, (x,y))
-
+#function to draw scenario by img path or other surface (like redline)
 def draw_scenario(surface, x, y, image_path = '', surface_target = ''):
     if surface_target == '':
         new_surface = pygame.image.load(image_path).convert()
@@ -32,7 +28,7 @@ def draw_scenario(surface, x, y, image_path = '', surface_target = ''):
     else:
         surface.blit(surface_target, (x,y))
 
-
+# function to draw text on screen 
 def draw_text(surface, text, color, x, y):
     text_str = font.render(text, True, color)
     surface.blit(text_str, (x,y))
@@ -78,7 +74,7 @@ screen_height = 720
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("comoqcaR")
 
-# background = pygame.image.load("assets/background.png").convert()
+fullscreen = False
 
 colors = ["azul", "vermelho", "verde", "roxo", "cinza"]
 # random_color = random.choice(colors)
@@ -90,29 +86,13 @@ font = pygame.font.Font('freesansbold.ttf', 32)
 pygame.time.set_timer(pygame.USEREVENT, 1000)
 
 SPAWN_CAR = pygame.USEREVENT + 1
-pygame.time.set_timer(SPAWN_CAR, random.randint(5000, 10000))
+# pygame.time.set_timer(SPAWN_CAR, random.randint(5000, 10000))
 
 game_time = 90
 
 score = 0
 
 actual_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") # chamar qd salvar o score, e fechar a tela do jogo
-
-# nome_arquivo = "dados_paciente.txt"
-
-# # Abrir o arquivo de texto no modo de escrita
-# with open(nome_arquivo, 'w') as arquivo:
-#     # Escrever o nome do paciente
-#     arquivo.write(f"Nome do Paciente: {nome_paciente}\n")
-    
-#     # Escrever os scores
-#     arquivo.write("Scores:\n")
-#     for score in scores:
-#         arquivo.write(f"{score}\n")
-    
-#     # Escrever a data atual
-#     arquivo.write(f"Data Atual: {data_atual}\n")
-
 
 car_colors = ["assets/car-blue.png", "assets/car-red.png", "assets/car-green.png", "assets/car-purple.png",  "assets/car-gray.png"]
 expected_color_path = car_colors[2]
@@ -140,7 +120,6 @@ sin_moviment = True
 
 # easy_mode_lines = [490, 790]
 # medium_mode_lines = [540, 740]
-
 hard_mode_lines = [550, 720]
 
 easy_gap = 150
@@ -149,16 +128,26 @@ hard_gap = 60
 
 dot_spacing = 20
 
+menu_text_x = 600
+menu_text_y = 150
+
 redline = create_redlines(screen_width, screen_height, dot_spacing, hard_gap)
+
+game_state = ''
+selected_option = 0 # index of menu_options
+menu_options = ['start', 'about', 'score', 'quit']
+menu_options_gap = 70
+
+space_pressed = False
+paused = False
+running = True
+
+game_state = GameState.change_state(game_state, GameState.menu)
 
 for _ in range(num_cars):
     random_car_color = random.choice(car_colors)
     check_green("green")
     car = create_car(random_car_color)
-
-space_pressed = False
-paused = False
-running = True
 
 while running:
     clock.tick(tickrate)
@@ -167,56 +156,95 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                for car in cars:
-                    if car.color == expected_color and (car.rect.x > hard_mode_lines[0] and car.rect.x <= hard_mode_lines[1]) and not space_pressed:  
-                        print(car.rect.x)
-                        space_pressed = True
-                        score += 1
-            elif event.key == pygame.K_CAPSLOCK:
-                paused = not paused
-                    
-        elif event.type == SPAWN_CAR:
-            if not paused: 
-                create_car("assets/car-green.png") 
-                pygame.time.set_timer(SPAWN_CAR, random.randint(5000, 7000)) 
-                space_pressed = False
-                print('[SPAWN]', seconds_to_min(game_time))
 
-        elif event.type == pygame.USEREVENT:
-            if not paused: 
-                game_time -= 1
-                if game_time <= 0:
+        elif game_state == GameState.menu:
+
+            draw_scenario(screen, 0, 0, 'assets/menu.png')
+            
+            for i, option in enumerate(menu_options):
+                if i == selected_option:
+                    draw_text(screen, option, (255, 0, 0), menu_text_x, menu_text_y + i * menu_options_gap)
+                else:
+                    draw_text(screen, option, (0, 0, 0), menu_text_x, menu_text_y + i * menu_options_gap)
+            
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_UP]:
+                selected_option = (selected_option - 1) % len(menu_options)
+            elif keys[pygame.K_DOWN]:
+                selected_option = (selected_option + 1) % len(menu_options)
+
+            if event.type == pygame.KEYDOWN and (event.key == pygame.K_RETURN or event.key == pygame.K_SPACE):
+                if selected_option == 0:
+                    game_state = GameState.change_state(game_state, GameState.game)
+                elif selected_option == 1:
+                    game_state = GameState.change_state(game_state, GameState.about)
+                elif selected_option == 2:
+                    game_state = GameState.change_state(game_state, GameState.score)
+                elif selected_option == 3:
                     running = False
+
+        elif game_state == GameState.game:
+
+            draw_scenario(screen, 0, 0, 'assets/background.png')
+            draw_scenario(screen, screen_width/2 + 5, 0, 'assets/background.png')
+            draw_scenario(screen, 0, 0, '', redline)
+
+            pygame.time.set_timer(SPAWN_CAR, random.randint(5000, 10000))
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_f:
+                    fullscreen = not fullscreen
+                    if fullscreen:
+                        screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
+                    else:
+                        screen = pygame.display.set_mode((screen_width, screen_height))
+
+                if event.key == pygame.K_SPACE:
+                    for car in cars:
+                        if car.color == expected_color and (car.rect.x > hard_mode_lines[0] and car.rect.x <= hard_mode_lines[1]) and not space_pressed:  
+                            print(car.rect.x)
+                            space_pressed = True
+                            score += 1
+                if event.key == pygame.K_CAPSLOCK:
+                    paused = not paused
+                        
+            elif event.type == SPAWN_CAR:
+                if not paused: 
+                    create_car("assets/car-green.png") 
+                    pygame.time.set_timer(SPAWN_CAR, random.randint(5000, 7000)) 
+                    space_pressed = False
+                    print('[SPAWN]', seconds_to_min(game_time))
+
+            elif event.type == pygame.USEREVENT:
+                if not paused: 
+                    game_time -= 1
+                    if game_time <= 0:
+                        running = False
                
                              
-    if not paused:
-        draw_scenario(screen, 0, 0, 'assets/background.png')
-        draw_scenario(screen, screen_width/2 + 5, 0, 'assets/background.png')
-        draw_scenario(screen, 0, 0, '', redline)
-        
-        for car in cars:
-            car.move()
+            if not paused:
+                
+                for car in cars:
+                    car.move()
 
-            if car.rect.x > screen_width + 121 or car.rect.x < -301: # 300 is the first possible spawn in bottom AND screen_width + 120 is the last possible spawn in top
-                cars.remove(car)
-                car_count -= 1
-                random_car_color = random.choice(car_colors)
-                check_green("green")
-                new_car = create_car(random_car_color) 
+                    if car.rect.x > screen_width + 121 or car.rect.x < -301: # 300 is the first possible spawn in bottom AND screen_width + 120 is the last possible spawn in top
+                        cars.remove(car)
+                        car_count -= 1
+                        random_car_color = random.choice(car_colors)
+                        check_green("green")
+                        new_car = create_car(random_car_color) 
 
-            if car_count < num_cars: # force to always have <num_cars> cars on the screen
-                random_car_color = random.choice(car_colors)
-                check_green("green")
-                new_car = create_car(random_car_color)
-            
-            check_sin_move(sin_moviment)
-            car.draw(screen)   
+                    if car_count < num_cars: # force to always have <num_cars> cars on the screen
+                        random_car_color = random.choice(car_colors)
+                        check_green("green")
+                        new_car = create_car(random_car_color)
+                    
+                    check_sin_move(sin_moviment)
+                    car.draw(screen)   
 
-        draw_text(screen, "aperte 'espaço' quando o carro " + random_color + " passar pela area vermelha", 'white', 200, 20)
-        draw_text(screen, seconds_to_min(game_time), 'white', 300, 90)
-        draw_text(screen, str(score), 'white', 300, 120)
+                draw_text(screen, "aperte 'espaço' quando o carro " + random_color + " passar pela area vermelha", 'white', 200, 20)
+                draw_text(screen, seconds_to_min(game_time), 'white', 300, 90)
+                draw_text(screen, str(score), 'white', 300, 120)
 
         pygame.display.flip()
 
--- states/gameState.py
+++ states/gameState.py
@@ -1,37 +1,14 @@
-import pygame
+from enum import Enum
 
-class GameState:
-    def __init__(self):
-        self.state = "menu" 
 
-    def handle_events(self, events):
-        for event in events:
-            if event.type == pygame.KEYDOWN:
-                if self.state == "menu":
-                    self.state = "game"  
-                elif self.state == "game":
-                    self.state = "score"  
-                elif self.state == "score":
-                    self.state = "menu" 
-                elif self.state == "score":
-                    self.state = "menu" 
-                elif self.state == "score":
-                    self.state = "menu" 
+class GameState(Enum):
+    menu = 1
+    game = 2
+    game_over = 3
+    about = 4
 
-    def draw(self, screen):
-        screen.fill((255,255,255))
-        if self.state == "start":
-            background = pygame.transform.scale2x(pygame.image.load("assets/bg.jpg")).convert()
-            font = pygame.font.Font(None, 36)
-            text = font.render("Pressione uma tecla para iniciar", True, (0, 0, 0))
-            screen.blit(background, (0, 0))
-            screen.blit(text, (250, 250))
-        elif self.state == "game":
-            font = pygame.font.Font(None, 36)
-        elif self.state == "score":
-            font = pygame.font.Font(None, 36)
-        elif self.state == "score":
-            font = pygame.font.Font(None, 36)
-        elif self.state == "score":
-            font = pygame.font.Font(None, 36)
+    def change_state(gameState, newState):
+        return newState
+
+ 
             
